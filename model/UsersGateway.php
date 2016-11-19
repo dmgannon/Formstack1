eb6e2c6540480b6dd97ff5250b58d4dbe88cef25
danielle <dmgannon@mail.plymouth.edu>
2016-11-19 15:52:47 -0500

    Changes tabs to 4 spaces. Fixed braces. PSR-2

--- model/UsersGateway.php
+++ model/UsersGateway.php
@@ -2,34 +2,55 @@
 
 require_once 'model/UsersService.php';
 
-class UsersGateway {
-     
-    //this function is done and working
-    public function selectAll($connection) {
+class UsersGateway
+{     
+    /**
+    * Function that selects all the users.
+    *
+    * Selects all the user data from the db
+    * to be sent back as $users array
+    */
+    public function selectAll($connection)
+    {
         $dbres = mysqli_query($connection, "SELECT * FROM users"); 
         $users = array();
-        while ( ($obj = mysqli_fetch_object($dbres)) != NULL ) {
+        while (($obj = mysqli_fetch_object($dbres)) != NULL)
+        {
             $users[] = $obj;
         }
-         
         return $users;
     }
      
-    //this function is done and working
-    public function selectById($id, $conn) {
+    /**
+    * Function that selects a specific user.
+    *
+    * Selects all the user data from the db
+    * from a specific user by user id.
+    */
+    public function selectById($id, $conn)
+    {
         $dbId = mysqli_real_escape_string($conn, $id);
 
-        if (!mysqli_query($conn, "SELECT * FROM users WHERE id=$dbId")) {
+        if (!mysqli_query($conn, "SELECT * FROM users WHERE id=$dbId"))
+        {
             printf("Errormessage: %s\n", mysqli_error($conn));
-        }else{
+        }else
+        {
             $dbres = mysqli_query($conn,"SELECT * FROM users WHERE id=$dbId");
         }
 
         return mysqli_fetch_object($dbres);
     }
      
-    //this function is done and complete
-    public function insert($email, $first_name, $last_name, $password, $conn) {
+    /**
+    * Function that inserts a user to the db.
+    *
+    * Receives the email, first name, last name, and password
+    * that is required to be entered in the db.
+    * All saved a strings and checked if null.
+    */
+    public function insert($email, $first_name, $last_name, $password, $conn)
+    {
         $emailString = mysqli_real_escape_string($conn, $email);
         $firstString = mysqli_real_escape_string($conn, $first_name);
         $lastString = mysqli_real_escape_string($conn, $last_name);
@@ -40,23 +61,37 @@ class UsersGateway {
         $dbLast = ($last_name != NULL)?"'".$lastString."'":'NULL';
         $dbPassword = ($password != NULL)?"'".$passString."'":'NULL';
 
-        if (!mysqli_query($conn, "INSERT INTO users (email, first_name, last_name, password) VALUES ($dbEmail, $dbFirst, $dbLast, $dbPassword)")) {
+        if (!mysqli_query($conn, "INSERT INTO users (email, first_name, last_name, password) VALUES ($dbEmail, $dbFirst, $dbLast, $dbPassword)"))
+        {
             printf("Errormessage: %s\n", mysqli_error($conn));
         }
 
         return mysqli_insert_id($conn);
     }
      
-    //this function is complete and working
-    public function delete($id, $conn) {
+    /**
+    * Function that deletes a specific user.
+    *
+    * Deletes a specific user from the db
+    * by the selected user id.
+    */
+    public function delete($id, $conn)
+    {
         $dbId = mysqli_real_escape_string($conn, $id);
-        if (!mysqli_query($conn, "DELETE FROM users WHERE id=$dbId")) {
+        if (!mysqli_query($conn, "DELETE FROM users WHERE id=$dbId"))
+        {
             printf("Errormessage: %s\n", mysqli_error($conn));
         }
     }
 
-    //this function is complete and working
-    public function edit($id, $email, $first_name, $last_name, $password, $conn) {
+    /**
+    * Function that edits a specific user.
+    *
+    * Receives the data required to edit a specific user.
+    * Updates the database after checking the data strings.
+    */
+    public function edit($id, $email, $first_name, $last_name, $password, $conn)
+    {
         $dbId = mysqli_real_escape_string($conn, $id);
         $emailString = mysqli_real_escape_string($conn, $email);
         $firstString = mysqli_real_escape_string($conn, $first_name);
@@ -68,12 +103,12 @@ class UsersGateway {
         $dbLast = ($last_name != NULL)?"'".$lastString."'":'NULL';
         $dbPassword = ($password != NULL)?"'".$passString."'":'NULL';
 
-        if (!mysqli_query($conn, "UPDATE users SET email=$dbEmail, first_name=$dbFirst, last_name=$dbLast, password=$dbPassword WHERE id=$dbId")) {
+        if (!mysqli_query($conn, "UPDATE users SET email=$dbEmail, first_name=$dbFirst, last_name=$dbLast, password=$dbPassword WHERE id=$dbId"))
+        {
             printf("Errormessage: %s\n", mysqli_error($conn));
         }
         return mysqli_insert_id($conn);
     }
-     
 }
  
 ?>
--- model/UsersService.php
+++ model/UsersService.php
@@ -3,91 +3,164 @@
 require_once 'model/UsersGateway.php';
 require_once 'model/ValidationException.php';
 
-class UsersService{
+class UsersService
+{
+    private $usersGateway = NULL;
 
-	private $usersGateway = NULL;
+    /**
+    * Function to connect to the database.
+    */
+    private function connectDb()
+    {
+        $connection = mysqli_connect("localhost", "my_app", "secret", "my_app");
+        return $connection;
+    }
 
-	private function connectDb(){
-		$connection = mysqli_connect("localhost", "my_app", "secret", "my_app");
-		return $connection;
-		//add warning for connection
-	}
-	private function closeDb($connection){
-		$connection->close();
-	}
-	public function __construct(){
-		$this->usersGateway = new UsersGateway();
-	}
-	public function getAllUsers($order){
-		try{
-			$conn = $this->connectDb();
-			$results=$this->usersGateway->selectAll($conn);
-			$this->closeDb($conn);
-			return $results;
-		}catch(Exception $e){
-			$this->closeDb();
-			throw $e;
-		}
-	}
-	public function getUser($id){
-		try {
+    /**
+    * Function to close to the database.
+    */
+    private function closeDb($connection)
+    {
+        $connection->close();
+    }
+
+    /**
+    * Constructs a new instance of UsersGateway
+    *
+    * This is to be used to call specific queries
+    */
+    public function __construct()
+    {
+        $this->usersGateway = new UsersGateway();
+    }
+
+    /**
+    * Function that selects all users from database.
+    *
+    * Connects to the DB through the "connectDb" function
+    * and closes through the "closeDb" function.
+    * Uses the "selectAll" function from UsersGateway
+    * to pull results from all users.
+    */
+    public function getAllUsers()
+    {
+        try
+        {
+            $conn = $this->connectDb();
+            $results=$this->usersGateway->selectAll($conn);
+            $this->closeDb($conn);
+            return $results;
+        }catch(Exception $e)
+        {
+            $this->closeDb();
+            throw $e;
+        }
+    }
+
+    /**
+    * Function that gets user data.
+    *
+    * Uses "selectById" function from UsersGateway
+    * to pull results of a single user.
+    */
+    public function getUser($id)
+    {
+        try
+        {
             $conn = $this->connectDb();
             $results = $this->usersGateway->selectById($id, $conn);
             $this->closeDb($conn);
             return $results;
-        } catch (Exception $e) {
+        } catch (Exception $e)
+        {
             $this->closeDb($conn);
             throw $e;
         }
         return $this->usersGateway->find($id);
-		
-	}
+    }
 
-	private function validateUserParams($email, $first_name, $last_name, $password){
-		$errors = array();
-		if(!isset($email)||empty($email)){
-			$errors[]='Email is required';
-		}
-		if (empty($errors)){
-			return;
-		}
-		throw new ValidationException($errors);
-	}
+    /**
+    * Function that validates user parameters.
+    *
+    * Collects email, first name, last name, and password
+    * and makes sure at least the email is entered.
+    * Can be changed to require all if wanted. Just simplicity for now.
+    */
+    private function validateUserParams($email, $first_name, $last_name, $password)
+    {
+        $errors = array();
+        if(!isset($email)||empty($email))
+        {
+            $errors[]='Email is required';
+        }
+        if (empty($errors))
+        {
+            return;
+        }
+        throw new ValidationException($errors);
+    }
 
-	public function createUser($email, $first_name, $last_name, $password){
-		try{
-			$conn = $this->connectDb();
-			$this->validateUserParams($email, $first_name, $last_name, $password);
-			$results = $this->usersGateway->insert($email, $first_name, $last_name, $password, $conn);
-			$this->closeDb($conn);
-			return $results;
-		}catch (Exception $e){
-			$this->closeDb($conn);
-			throw $e;
-		}
-	}
-	public function editUser($id, $email, $first_name, $last_name, $password){
-		try{
-			$conn = $this->connectDb();
-			$this->validateUserParams($email, $first_name, $last_name, $password);
-			$results = $this->usersGateway->edit($id, $email, $first_name, $last_name, $password, $conn);
-			$this->closeDb($conn);
-		}catch (Exception $e){
-			$this->closeDb($conn);
-			throw $e;
-		}
+    /**
+    * Function that creates a user.
+    *
+    * Collects user data from the form and sends
+    * it to function "insert" from UsersGateway.
+    * 
+    */
+    public function createUser($email, $first_name, $last_name, $password)
+    {
+        try
+        {
+            $conn = $this->connectDb();
+            $this->validateUserParams($email, $first_name, $last_name, $password);
+            $results = $this->usersGateway->insert($email, $first_name, $last_name, $password, $conn);
+            $this->closeDb($conn);
+            return $results;
+        }catch (Exception $e)
+        {
+            $this->closeDb($conn);
+            throw $e;
+        }
+    }
 
-	}
-	public function deleteUser($id){
-		try{
-			$conn = $this->connectDb();
-			$results = $this->usersGateway->delete($id, $conn);
-			$this->closeDb($conn);
-		}catch (Exception $e){
-			$this->closeDb($conn);
-			throw $e;
-		}
-	}
+    /**
+    * Function that edits a specific user.
+    *
+    * Collects user data entered in the edit form
+    * and sends it to the "edit" function from UsersGateway.
+    */
+    public function editUser($id, $email, $first_name, $last_name, $password)
+    {
+        try
+        {
+            $conn = $this->connectDb();
+            $this->validateUserParams($email, $first_name, $last_name, $password);
+            $results = $this->usersGateway->edit($id, $email, $first_name, $last_name, $password, $conn);
+            $this->closeDb($conn);
+        }catch (Exception $e)
+        {
+            $this->closeDb($conn);
+            throw $e;
+        }
+    }
 
-
+    /**
+    * Function that deletes a specific user.
+    *
+    * Collects the id of the selected user and
+    * sends it to the "delete" function in UsersGateway.
+    */
+    public function deleteUser($id)
+    {
+        try
+        {
+            $conn = $this->connectDb();
+            $results = $this->usersGateway->delete($id, $conn);
+            $this->closeDb($conn);
+        }catch (Exception $e)
+        {
+            $this->closeDb($conn);
+            throw $e;
+        }
+    }
 }
